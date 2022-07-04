bdafaff9a1c4f345e54ebeb34ec44d4fc495fe07
Alanis Rochet <alanis.brend@gmail.com>
2022-07-04 00:16:40 -0300

    agregando

--- General/taza-app/src/components/Card.js
+++ General/taza-app/src/components/Card.js
@@ -5,8 +5,13 @@ import './Card.css';
 function Card(props) {
   return (
     <div>
+        <div>
         <h3>{props.product}</h3>
         <p>{props.price}</p>
+        </div>
+        <div>
+          <button>Ver detalles del producto</button>
+        </div>
     </div>
   );
 }
